c29fd6253e5ba430ffc5477d8c56e699e5ba594e
Raphael Reyna <raphaelreyna@protonmail.com>
2021-08-15 19:00:34 -0700

    Add live reloading / dynamic schema

--- cmd/graphqld/main.go
+++ cmd/graphqld/main.go
@@ -6,9 +6,12 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sync"
+	"time"
 
 	"github.com/graphql-go/graphql"
 	"github.com/graphql-go/handler"
+	"github.com/radovskyb/watcher"
 	"github.com/raphaelreyna/graphqld/internal/graph"
 )
 
@@ -47,8 +50,59 @@ func main() {
 		log.Fatal(err)
 	}
 
+	w := watcher.New()
+	w.SetMaxEvents(1)
+	w.FilterOps(
+		watcher.Rename, watcher.Move,
+		watcher.Create, watcher.Chmod,
+		watcher.Write,
+	)
+
+	lock := sync.RWMutex{}
+	go func() {
+		for {
+			select {
+			case <-w.Event:
+				g := graph.Graph{
+					Dir: rootDir,
+				}
+
+				if err := g.Build(); err != nil {
+					log.Fatal(err)
+				}
+
+				schm, err := graphql.NewSchema(graphql.SchemaConfig{
+					Query: graphql.NewObject(g.Query.ObjectConf),
+				})
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				lock.Lock()
+				schema = schm
+				lock.Unlock()
+				fmt.Println("updated schema")
+
+			case err := <-w.Error:
+				log.Fatalln(err)
+			case <-w.Closed:
+				return
+			}
+		}
+	}()
+
+	if err := w.AddRecursive(g.Dir); err != nil {
+		log.Fatalln(err)
+	}
+	go func() {
+		if err := w.Start(time.Second); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
 	http.HandleFunc("/graphql", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		opts := handler.NewRequestOptions(r)
+		lock.RLock()
 		result := graphql.Do(graphql.Params{
 			Schema:         schema,
 			RequestString:  opts.Query,
@@ -56,6 +110,7 @@ func main() {
 			OperationName:  opts.OperationName,
 			Context:        r.Context(),
 		})
+		lock.RUnlock()
 		w.Header().Add("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(result)
 	}))
@@ -64,5 +119,6 @@ func main() {
 	if x := os.Getenv("PORT"); x != "" {
 		port = x
 	}
+	fmt.Println("starting...")
 	http.ListenAndServe(":"+port, nil)
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/raphaelreyna/graphqld
 go 1.16
 
 require (
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
 	github.com/graphql-go/graphql v0.7.9
 	github.com/graphql-go/handler v0.2.3
 	github.com/matryer/is v1.4.0
+	github.com/radovskyb/watcher v1.0.7 // indirect
 )
